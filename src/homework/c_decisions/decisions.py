82f2ec9293cf4ae1deb44fe61cbfa16dd5df5577
Joellemayoph <joelle.mayo@g.austincc.edu>
2025-02-20 02:29:14 +0000

    Homework 2

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,5 +1,20 @@
 #
-def get_options_ratio (option, total):
+def get_options_ratio(option, total):
     ratio = option / total
     return ratio
 
+def get_faculty_rating(ratio):
+        rating = 0
+
+        if(ratio >= .9 and ratio < 1):
+            rating = "Excellent"
+        elif(ratio >= .8 and ratio < .9):
+            rating = "Very Good"
+        elif(ratio >= .7 and ratio < .8):
+            rating = "Good"
+        elif(ratio >= .6 and ratio < .7):
+            rating = "Needs Improvement"
+        elif(ratio >=0 and ratio < .6):
+            rating = "Unacceptable"
+        
+        return rating
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,12 @@
-#
+import decisions
+
+def main(): 
+    num1 = input("Enter your options: ")
+    num2 = input("Enter your total: ")
+
+    opratio = decisions.get_options_ratio(int(num1), int(num2))
+    result = decisions.get_faculty_rating(opratio)
+
+    print(result)
+
+main()
