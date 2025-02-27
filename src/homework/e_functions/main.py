14eadb46fd28628d63b9218328623104d5315896
Joellemayoph <joelle.mayo@g.austincc.edu>
2025-02-27 03:12:11 +0000

    Homework 4

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,12 @@
-#
+#get_hour(epoch_seconds)
+
+from value_return import get_hour, get_minutes, get_seconds
+
+def main():
+    epoch_second = int(input('Enter seconds'))
+    hours = get_hour(epoch_second)
+    minutes = get_minutes(epoch_second)
+    seconds = get_seconds(epoch_second)
+
+    print(f'{hours:02d} : {minutes:02d} : {seconds:02d}')
+main()
--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,1 +1,16 @@
-#
+
+def get_hour(epoch_seconds):
+        hour = epoch_seconds // 3600
+        return hour
+
+def get_minutes(epoch_seconds):
+        minutes = (epoch_seconds // 60)%60
+        return minutes
+
+def get_seconds(epoch_seconds):
+        seconds = (epoch_seconds % 60)%60
+        return seconds
+
+            
+
+
